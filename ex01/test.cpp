329d97fd5ea2675250f917bf21bd44bdc42e2338
lmohin <loic.mohin@gmail.com>
2025-11-25 00:01:40 +0100

    adding linearAlgebra header to test file

--- ex01/test.cpp
+++ ex01/test.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "Vector.hpp"
-
-Vector	linearCombination(std::vector<Vector> const &vectors, std::vector<float> const &coefs);
+#include "linearAlgebra.hpp"
 
 int main(void)
 {
