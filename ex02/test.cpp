99ec5988a853c3feb906d4f652be9c796e10da14
lmohin <loic.mohin@gmail.com>
2025-11-25 00:02:44 +0100

    linearInterpolation done

---
+++ ex02/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Vector.hpp"
+#include "linearAlgebra.hpp"
+
+int main(void)
+{
+	Vector a({2, 1});
+	Vector b({0, 10, -100});
+	Vector c({4, 2});
+	Matrix aa({2, 1, 3, 4}, 2, 2);
+	Matrix bb({20, 10, 30, 40}, 2, 2);
+	std::vector<Vector> comb;
+	comb.push_back(a);
+	comb.push_back(b);
+	//comb.push_back(c);
+	std::cout << lerp(aa, bb, 0.5) << std::endl;
+}
